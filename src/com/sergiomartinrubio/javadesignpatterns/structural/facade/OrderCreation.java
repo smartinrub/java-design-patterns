8d91fd8b9da279941850387608427b1bbbdb4916
Sergio Martin Rubio <econsergio@gmail.com>
2020-04-05 20:16:20 +0100

    Clean up

--- src/com/sergiomartinrubio/javadesignpatterns/structural/facade/OrderCreation.java
+++ src/com/sergiomartinrubio/javadesignpatterns/structural/facade/OrderCreation.java
@@ -3,6 +3,7 @@ package com.sergiomartinrubio.javadesignpatterns.structural.facade;
 import java.util.UUID;
 
 public class OrderCreation implements Action {
+
     @Override
     public void doAction(UUID orderId) {
         System.out.println("Creating order " + orderId + " on the system.");
